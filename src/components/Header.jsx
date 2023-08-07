624269cf9108e3266fc208b1c882d6372eefd0d7
Braisarco <braisarco@gmail.com>
2023-08-07 22:45:51 +0200

    I modified a litle bit the header and the main menu, also i left the space to add some icons with links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,15 @@ function Header(){
 
 
     return (
-        <div className = "flex w-screen bg-gray-100 border-solid border-2 border-b-gray-700"> 
-            <div className = "w-full">
-                <p className = "font-bold p-8 text-5xl text-center">
+        <div className = "flex w-screen bg-black bg-opacity-40 border-b-solid border-b-2 border-white"> 
+            <div className = "w-2/5 p-8">
+                <p className = " font-bold text-5xl text-purple-500 text-center">
                     Brais Pérez {' '}
                     <span className = "text-blue-500">Portfolio</span>
                 </p> 
+            </div>
+            <div>
+
             </div>
         </div>
     )
--- src/components/MenuHorizontal.jsx
+++ src/components/MenuHorizontal.jsx
@@ -5,19 +5,19 @@ function MenuHorizontal(){
     
 
     return (
-        <div className = "bg-gray-100">
+        <div className = "bg-black bg-opacity-40">
             <button 
-                className = " w-1/3 p-4 uppercase font-semibold hover:bg-gray-400 "
+                className = "text-white w-1/3 p-4 uppercase font-semibold hover:bg-gray-400 "
                 //onClick = { setSeccion("aboutme") }
                 >About me
             </button>
             <button 
-                className = "w-1/3 p-4 border-solid border-2 border-x-black uppercase font-semibold hover:bg-gray-400"
+                className = "text-white w-1/3 p-4 border-x-solid border-x-2 border-x-white uppercase font-semibold hover:bg-gray-400"
                 //onClick = { setSeccion("post") }
                 >Post
             </button>
             <button 
-                className = "w-1/3 p-4 uppercase font-semibold hover:bg-gray-400"
+                className = "text-white w-1/3 p-4 uppercase font-semibold hover:bg-gray-400"
               //  onClick = { setSeccion("forecast") }
                 >Forecast
            </button>
